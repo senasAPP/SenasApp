d99af94ecffe318806c894aa6196e4e7d0280341
JulioCam01 <julio_mendoza2001@live.com>
2022-11-07 21:14:59 -0600

    Mi primer cambio
    Cambie los titulos de las pantallas, quite la descripcion y la barrita

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -66,7 +66,7 @@ function BottomTabNavigator() {
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
+          title: 'Inicio',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
@@ -88,7 +88,7 @@ function BottomTabNavigator() {
         name="TabTwo"
         component={TabTwoScreen}
         options={{
-          title: 'Tab Two',
+          title: '2do',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
