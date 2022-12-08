18b2adad3152f22454ab898dbf71913922d7cdc8
JulioCam01 <julio_mendoza2001@live.com>
2022-12-07 18:30:00 -0600

    speech to text
    en la screen 2 agregue speech to text

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,13 +1,52 @@
-import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button  } from 'react-native';
+import { useEffect, useState } from 'react';
+import Voice from '@react-native-voice/voice';
 
 export default function TabTwoScreen() {
+  let [started, setStarted] = useState(false);
+  let [results, setResults] = useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startSpeechToText = async () => {
+    await Voice.start('en-NZ');
+    setStarted(true);
+  };
+
+  const stopSpeechToText = async () => {
+    await Voice.stop();
+    setStarted(false);
+  };
+
+  const onSpeechResults = (results) => {
+    setResults(results.value);
+  };
+
+  const onSpeechError = (error) => {
+    console.log(error);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Segunda</Text>      
+      {!started ? (
+        <Button title='Start Speech to Text' onPress={startSpeechToText} />
+      ) : undefined}
+      {started ? (
+        <Button title='Stop Speech to Text' onPress={stopSpeechToText} />
+      ) : undefined}
+      {results.map((result, index) => (
+        <Text key={index}>{result}</Text>
+      ))}
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,16 +54,8 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
